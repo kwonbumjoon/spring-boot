8829bee52583bbaaa58730560dc46e590514a07d
Bumjoon Kwon <bumjoon1@naver.com>
2024-04-05 09:44:53 +0900

    first commit

---
+++ SampleTest/src/main/java/kr/com/ezen/persistance/MapperTests.java
@@ -0,0 +1,83 @@
+package kr.com.ezen.persistance;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import kr.com.ezen.dto.MemberVO;
+import kr.com.ezen.mapper.MemberMapper;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class MapperTests {
+
+	@Autowired
+	private MemberMapper memberMapper;
+	
+//	@Test
+//	public void testGetTime() {
+//		log.info("-------------------------------");
+//		log.info(memberMapper.getClass().getName());
+//		log.info(memberMapper.getTime());
+//		log.info("-------------------------------");
+//		
+//	}
+	
+	@Test
+	public void testInsert() {
+//		MemberVO vo = new MemberVO();
+//		vo.setId(0);
+//		vo.setName("조조");
+//		vo.setPhone("010-1111-2222");
+//		vo.setAddress("경기도 화성시");
+//		memberMapper.insertMember(vo);
+		
+		for(int i=0; i<10; i++) {
+			MemberVO vo = MemberVO.builder()
+					.id(10+i)
+					.name("user" + i)
+					.phone("010-1111-222" + i)
+					.address("서울시 장안구 " + i)
+					.build();
+			memberMapper.insertMember(vo);
+		}
+	}
+	
+	@Test
+	public void testUpdate() {
+			MemberVO vo = MemberVO.builder()
+					.id(9)
+					.name("조운")
+					.phone("010-1111-2222")
+					.address("경기도 수원시")
+					.build();
+			
+			memberMapper.insertMember(vo);
+	}
+	
+	@Test
+	public void testDelete() {
+		memberMapper.deleteMember(19);
+	}
+	
+	@Test
+	public void testSelectOne() {
+		MemberVO vo = memberMapper.selectOneMember(17);
+		log.info(">>>> " + vo);
+	}
+	
+	@Test
+	public void testAllList() {
+//		List<MemberVO> list = memberMapper.selectAllList();
+//		for(MemberVO vo : list)
+//			log.info(vo);
+		
+		memberMapper.selectAllList().forEach(vo -> log.info(vo));
+	}
+}
---
+++ SampleTest2/src/main/java/kr/com/ezen/persistance/JDBCConnectionTest.java
@@ -0,0 +1,37 @@
+package kr.com.ezen.persistance;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class JDBCConnectionTest {
+	static {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+//	junit test를 위해 사용한 어노테이션
+	@Test
+	public void testConnection() {
+		try {
+			Connection con = DriverManager.getConnection(
+					"jdbc:oracle:thin:@localhost:1521:xe",
+					"test2",
+					"1234"
+					);
+//			출력시 원하는 레벨로 설정하여 필요없는 에러는 안보이게 끔 설정 및 출력
+			log.info(con);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
