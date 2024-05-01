c1474e976ad1997ace0c025d44826256a11d4d9d
Bumjoon Kwon <bumjoon1@naver.com>
2024-05-01 09:16:12 +0900

    first commit

--- ex06_security2/src/test/java/org/zerock/security/PasswordTest.java
+++ ex06_security2/src/test/java/org/zerock/security/PasswordTest.java
@@ -1,11 +1,19 @@
 package org.zerock.security;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.AuthVO;
+import org.zerock.domain.MemberVO;
+import org.zerock.mapper.MemberMapper;
 
 import lombok.extern.log4j.Log4j;
 
@@ -16,21 +24,68 @@ import lombok.extern.log4j.Log4j;
 public class PasswordTest {
 
 	@Autowired
-	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	private BCryptPasswordEncoder pwEncoder;
+
+//	@Autowired
+//	private DataSource source;
+	
+	@Autowired
+	private MemberMapper memberMapper;
 	
 	@Test
+	public void TestInsertMember() {
+
+		for(int i=1; i<=100; i++) {
+			MemberVO memberVO = new MemberVO();
+			
+			memberVO.setUserid("user" + i);
+			memberVO.setUserpw(pwEncoder.encode("1111"));
+			memberVO.setUsername("USER" + i);
+			
+			List<AuthVO> authList = new ArrayList<AuthVO>();
+			
+			AuthVO userAuth = new AuthVO("user" + i, "ROLE_USER");
+			authList.add(userAuth);
+			
+			if(i > 50) {
+				AuthVO mamagerAuth = new AuthVO("user" + i, "ROLE_MEMBER");
+				authList.add(mamagerAuth);
+			}
+			
+			if(i >= 90) {
+				AuthVO adminAuth = new AuthVO("user" + i, "ROLE_ADMIN");
+				authList.add(adminAuth);
+			}
+			
+			memberVO.setAuthList(authList);
+			
+			memberMapper.insert(memberVO);
+			
+			authList.stream().forEach(authVO -> {
+				memberMapper.insertAuth(authVO);
+			});
+		}
+	}
+
+	@Test
 	public void testPassword() {
-		log.info("bCryptPasswordEncoder : " + bCryptPasswordEncoder);
-		
+		log.info("bCryptPasswordEncoder : " + pwEncoder);
+
 		String str = "1234";
-		
-		String encodeStr = bCryptPasswordEncoder.encode(str);
-		
+
+		String encodeStr = pwEncoder.encode(str);
+
 		log.info("-----------------------------------------------");
 		log.info(encodeStr);
-		
-		boolean match = bCryptPasswordEncoder.matches(str, encodeStr);
+
+		boolean match = pwEncoder.matches(str, encodeStr);
 		log.info("-----------------------------------------------");
 		log.info(match);
+	}
+	
+	@Test
+	public void testRead() {
+		MemberVO vo = memberMapper.read("user99");
+		log.info(vo);
 	}
 }
